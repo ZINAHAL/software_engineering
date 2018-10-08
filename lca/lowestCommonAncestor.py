8e4cb090bb7f3d9b103c5912ef2c10d9774ba203
ZINAHAL <albaghdz@tcd.ie>
2018-10-08 11:40:08 +0100

    added tests for when all/some given nodes are present in the tree

--- lca/lowestCommonAncestor.py
+++ lca/lowestCommonAncestor.py
@@ -6,30 +6,44 @@
 # n1 and n2 
 
 # A Binary tree node 
-class Node: 
-	# Constructor to create a new node 
-	def __init__(self, data): 
-		self.data = data 
-		self.left = None
-		self.right = None
+
+class Node:
+    # Constructor to create a new node
+    def __init__(self, key, data):
+        self.key = key
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def get_data(root, given_key):
+    if root is None:
+        return None
+    if given_key < root.key:
+        return get_data(root.left, given_key)
+    if given_key > root.key:
+        return get_data(root.right, given_key)
+    return root.data
+    
+
 
 # Function to find LCA of n1 and n2. The function assumes 
-# that both n1 and n2 are present in BST 
-def lca(root, n1, n2): 
+# that both n1(k1) and n2(k2) are present in BST 
+def lca(root, k1, k2): 
 	
 	# Base Case 
-	if root is None: 
+	if (root is None) or (get_data(root, k1) is None) or (get_data(root, k2) is None): 
 		return None
 
-	# If both n1 and n2 are smaller than root, then LCA 
+	# If both n1(k1) and n2(k2) are smaller than root, then LCA 
 	# lies in left 
-	if(root.data > n1 and root.data > n2): 
-		return lca(root.left, n1, n2) 
+	if(root.key > k1 and root.key > k2): 
+		return lca(root.left, k1, k2) 
 
-	# If both n1 and n2 are greater than root, then LCA 
+	# If both n1(k1) and n2(k2) are greater than root, then LCA 
 	# lies in right 
-	if(root.data < n1 and root.data < n2): 
-		return lca(root.right, n1, n2) 
+	if(root.key < k1 and root.key < k2): 
+		return lca(root.right, k1, k2) 
 
 	return root 
 
--- lca/test_lca.py
+++ lca/test_lca.py
@@ -1,21 +1,36 @@
-from lowestCommonAncestor import Node
+from lowestCommonAncestor import Node, lca, get_data
 
 root = None
 
 #this function will be executed first before all the test functions
 def setup_module():
-    print("\nseting up the Binary Search Tree to test for LCA...")
     global root
-    root = Node(20) 
-    root.left = Node(8) 
-    root.right = Node(22) 
-    root.left.left = Node(4) 
-    root.left.right = Node(12) 
-    root.left.right.left = Node(10) 
-    root.left.right.right = Node(14)
-    print("the tree is now complete...")
+    root = Node(20, 'a') 
+    root.left = Node(8, 'b') 
+    root.right = Node(22, 'c') 
+    root.left.left = Node(4, 'd') 
+    root.left.right = Node(12, 'e') 
+    root.left.right.left = Node(10, 'f') 
+    root.left.right.right = Node(14, 'g')
 
 
+def test_when_given_nodes_are_present():
+    global root
+    assert lca(root, 10, 14).key == 12
+    assert lca(root, 4, 14).key == 8
+    assert lca(root, 22, 14).key == 20
+    assert lca(root, 4, 22).key == 20
+    assert lca(root, 20, 20).key == 20
+    assert lca(root, 14, 14).key == 14
+    assert lca(root, 20, 14).key == 20
+    assert lca(root, 14, 8).key == 8
+
+
+def test_when_some_given_nodes_are_present():
+    assert lca(root, 999, 14) == None
+    assert lca(root, 8, 999) == None
+    assert lca(root, 999, 20) == None
+    
 
 #this function will be executed last after all the test functions are completed
 def teardown_module():
