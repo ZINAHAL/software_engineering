bbb17bc4fe58fb62575a8721ade7f30586921661
ZINAHAL <albaghdz@tcd.ie>
2018-12-02 14:49:08 +0000

    modified the function from gathering the languages of all the repos to only gathering info of repos that were created by the user i.e not forked ones

--- the_github_project/githubdrv.py
+++ the_github_project/githubdrv.py
@@ -5,15 +5,16 @@ class DataGatherer():
 
     def __init__(self, username):
         self.username = username
-        self.g = Github()
+        self.g = Github("***REMOVED***", "***REMOVED***")
         
 
     def gather_used_languages_in_repos(self):
         languagesUsed = {}
 
         for repo in self.g.get_user(self.username).get_repos():
-            for key, value in repo.get_languages().items():
-                value = languagesUsed.get(key, 0) + value
-                languagesUsed.update({key: value})
+            if not repo.fork:
+                for key, value in repo.get_languages().items():
+                    value = languagesUsed.get(key, 0) + value
+                    languagesUsed.update({key: value})
 
         return languagesUsed
